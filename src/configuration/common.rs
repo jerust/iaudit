5281432ccf9f53afae4dbaac79abd4e29d46a513
jerust <jerust@163.com>
2024-12-11 00:24:34 +0800

    add scenario collection configuration

--- src/configuration/common.rs
+++ src/configuration/common.rs
@@ -2,6 +2,9 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct CommonSettings {
+    pub lower_threshold: usize,
     pub thinktank_cache: String,
+    pub thinktank_space: String,
     pub guideline_cache: String,
+    pub guideline_space: String,
 }
